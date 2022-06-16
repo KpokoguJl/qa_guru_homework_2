3f0d9853c4a761d2d35dd2aaee9b0b59b4bd25b8
Chikov <chekistne@mail.ru>
2022-06-16 20:28:19 +0300

    Added positive and negative test on google search

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -1,7 +1,28 @@
+from sys import modules
+
+import pytest
 from selene.support.shared import browser
 from selene import be, have
 
+from conftest import browser_settings
 
-browser.open('https://google.com')
-browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
-browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+@pytest.fixture(scope='session', autouse=True)
+def before_all(browser_settings):
+    print('Браузер сконфигурирован!')
+
+
+@pytest.fixture(autouse=True)
+def before_each(browser_settings):
+    browser.open('/')
+    print('Открыта страницп!')
+
+
+def test_valid_search():
+    browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
+    browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
+
+
+def test_invalid_search():
+    browser.element('[name="q"]').should(be.blank).type('gjksdhgklhjsdfgkljdfshgklsdfjglfksdghlksdfghldfksgh').press_enter()
+    browser.element('[class="card-section"]').should(have.text('gjksdhgklhjsdfgkljdfshgklsdfjglfksdghlksdfghldfksgh'))
